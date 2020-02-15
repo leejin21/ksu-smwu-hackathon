14d5635c0825100bfcf20d8852859ba5d598febe
jeanstea <leejin21c@gmail.com>
2020-02-15 23:10:39 +0900

    Changed png file bc of the copyright

--- toy/urls.py
+++ toy/urls.py
@@ -6,7 +6,7 @@ urlpatterns = [
     path('posts', posts),
     # path('', views.post_list, name = 'post_list'),
     path('', views.main, name = 'main'),
-    path('main.css', views.main, name = 'main.css'),
+    # path('main.css', views.main, name = 'main.css'),
     path('page2/', views.page2, name = "page2"),
     path('page2/page3/', views.page3, name = "page3"),
 ]
